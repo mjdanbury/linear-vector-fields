122696d75a8ab603d212170851a43918cc7c74f1
Matty Danbury <46234082+mjdanbury@users.noreply.github.com>
2024-09-29 23:09:29 -0400

    get p5-svelte working

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,56 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import P5 from 'p5-svelte';
+	let width = 55;
+	let height = 55;
+
+	const sketch = (p5) => {
+		p5.setup = () => {
+			p5.createCanvas(400, 400);
+		};
+
+		p5.draw = () => {
+			p5.ellipse(p5.width / 2, p5.height / 2, width, height);
+		};
+	};
+</script>
+
+<div class="body">
+	<div class="sketch">
+		<P5 {sketch} debug />
+	</div>
+
+	<div class="slider">
+		Width
+		<input type="range" bind:value={width} min="100" max="1000" step="0.01" />
+		{width}
+	</div>
+	<div class="slider">
+		Height
+		<input type="range" bind:value={height} min="100" max="1000" step="0.01" />
+		{height}
+	</div>
+</div>
+
+<style>
+	.body {
+		/* background: red; */
+		width: 600px;
+		margin: 0 auto;
+		display: flex;
+		flex-direction: column;
+		align-self: center;
+		justify-content: center;
+	}
+	.slider {
+		/* background: orange; */
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		margin: 10px;
+	}
+	.sketch {
+		/* background-color: yellow; */
+		display: flex;
+		justify-content: center;
+	}
+</style>
